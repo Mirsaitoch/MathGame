037c6f4697271407790a7cbbbbb48e47f0577159
Mirsait Sabirzyanov <mirsait77777@gmail.com>
2023-11-10 15:41:48 +0300

    Initial Commit

---
+++ MathGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MathGame
+//
+//  Created by Мирсаит Сабирзянов on 10.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MathGame/MathGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathGameApp.swift
+//  MathGame
+//
+//  Created by Мирсаит Сабирзянов on 10.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MathGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
